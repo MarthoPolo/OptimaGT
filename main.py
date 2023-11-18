5c0f09b3be4df78e85fd19ae3c213fc60a864033
Dhruv Chaudhari <70984112+dhruvchaudhari05@users.noreply.github.com>
2023-11-18 01:53:23 -0500

    React frontend
    Added React project into repo for frontend

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from bs4 import BeautifulSoup
 import requests
-URL = 'http://www.values.com/inspirational-quotes'
+URL = 'https://www.myprotein.co.in/blog/nutrition/bulking-how-to-bulk-tips-2/'
 r = requests.get(URL) 
 
 soup = BeautifulSoup(r.content, 'html5lib') # If this line causes an error, run 'pip install html5lib' or install html5lib 
-print(soup.prettify())
+data_str = ""
+for item in soup.find_all("p"):
+    data_str = data_str + item.get_text()
+       
+print(data_str)
