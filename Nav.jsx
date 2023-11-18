33a4b6856fb8374f1d8aa4c82605a818e29c49fb
paimasiko3 <paimasiko3@gatech.edu>
2023-11-18 12:52:38 -0500

    making new react app:

---
+++ Nav.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import './App.css';
+
+export const Nav = () => {
+    return (
+    //   <div className="w-[672px] h-[36px]">
+    //     <div className="fixed w-[676px] h-[36px] top-0 left-0">
+    //       <div className="relative w-[672px] h-[36px] bg-[#2c82c942]">
+    //         <div className="absolute top-[14px] left-[14px] [font-family:'Josefin_Sans-Regular',Helvetica] font-normal text-white text-[12px] tracking-[0] leading-[normal] whitespace-nowrap">
+    //           PersonaFitAI
+    //         </div>
+    //         <div className="absolute w-[62px] h-[26px] top-[9px] left-[587px] bg-[url(rectangle-7.svg)] bg-[100%_100%]">
+    //           <div className="absolute w-[38px] top-[3px] left-[15px] [font-family:'Josefin_Sans-Regular',Helvetica] font-normal text-[#424b54] text-[10px] tracking-[0] leading-[normal]">
+    //             Sign In
+    //           </div>
+    //         </div>
+    //       </div>
+    //     </div>
+    //   </div>
+    );
+  };
+
+
+export default Nav
