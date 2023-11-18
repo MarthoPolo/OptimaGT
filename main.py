6437a8f1bc87361a840024a5a519180e47abd34f
Dhruv Chaudhari <70984112+dhruvchaudhari05@users.noreply.github.com>
2023-11-18 10:25:33 -0500

    reading link data and writing to file
    added functionality to read from a text file, access each link, and write text scraped into a text file

--- main.py
+++ main.py
@@ -1,11 +1,32 @@
 from bs4 import BeautifulSoup
 import requests
-URL = 'https://www.myprotein.co.in/blog/nutrition/bulking-how-to-bulk-tips-2/'
-r = requests.get(URL) 
 
-soup = BeautifulSoup(r.content, 'html5lib') # If this line causes an error, run 'pip install html5lib' or install html5lib 
-data_str = ""
-for item in soup.find_all("p"):
-    data_str = data_str + item.get_text()
+file_path = 'fitness_links.txt'
+
+# Read fitness goals from the text file using readlines()
+with open(file_path, 'r') as file:
+    fitness_links = file.readlines()
+
+# Iterate through the list and store each goal in its own string, removing newline characters
+link_strings = [goal.strip() for goal in fitness_links]
+
+for link in link_strings:
+    URL = link
+    r = requests.get(URL) 
+
+    soup = BeautifulSoup(r.content, 'html5lib') # If this line causes an error, run 'pip install html5lib' or install html5lib 
+    data_str = ""
+    for item in soup.find_all("p"):
+        data_str = data_str + item.get_text()
+
+    # Specify the path to the output text file
+    output_file_path = 'raw_text.txt'
+
+    # Write formatted fitness goals to the output text file
+    with open(output_file_path, 'w') as output_file:
+        output_file.write(data_str)
+
+    
        
-print(data_str)
+
+
